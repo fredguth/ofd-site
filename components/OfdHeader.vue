512755bf52577c5398ab05994205978a159f26db
Caê Penna <caepenna@gmail.com>
2019-02-15 14:46:28 -0200

    Refact OfdHeader style

--- components/OfdHeader.vue
+++ components/OfdHeader.vue
@@ -1,9 +1,9 @@
 <template lang="pug">
-
-header.ofd-header#ofd-header(:class="{ '-fix': showHeader }")
-  a.logo(href="/") <span>One</span> Final Doc
-  nav.nav
-    a.ofd-button(href="#signup") Get your invitation
+  header.ofd-header(:class="{ '-fix': showHeader }")
+    a.logo(href="/") <span>One</span> Final Doc
+    nav.nav
+      a.ofd-button.-transparent(href="#signin") Sign in
+      a.ofd-button(href="#signup") Get your invitation
 </template>
 
 <script>
@@ -42,5 +42,50 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="sass">
-// component sass could be here.  with import of mixins
+  @import "assets/styles/ambience"
+  .ofd-header
+    +container-width
+    background-color: transparent
+    position: absolute
+    top: 0
+    right: 0
+    left: 0
+    display: flex
+    justify-content: space-between
+    height: $space-deca
+    align-items: center
+    opacity: 1
+    z-index: 100
+    transition: none
+    z-index: 9
+
+    > .logo
+      opacity: 0
+      font-weight: 700
+      +typography-sizes($font-size-base, $line-height-deci)
+      > span
+        position: relative
+        // TODO: Refact border-bottom to pseudo-element so it can be positioned
+        border-bottom: 2px solid currentColor
+
+    &.-fix
+      background-color: $color-ofd-yellow
+      box-shadow: 0 4px 12px rgba(#000, 0.1)
+      position: fixed
+      transition: all 0.6s ease
+      > .logo
+        opacity: 1
+
+    &.-hide
+      opacity: 0
+
+    > .nav > .ofd-button
+      margin-left: $space-milli
+      +media-mediumdown
+        padding-top: $space-micro
+        padding-bottom: $space-micro
+        padding-left: $space-micro
+        padding-right: $space-micro
+        font-size: $font-size-centi
+        line-height: $line-height-milli
 </style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -5,19 +5,5 @@
 </template>
 
 <style lang="sass">
-@import "assets/styles/ambience"
-
-// Base
-@import "assets/styles/base"
-
-// Components
-@import "assets/styles/components/ofd-button"
-@import "assets/styles/components/ofd-header"
-@import "assets/styles/components/section-problem"
-@import "assets/styles/components/section-solution"
-@import "assets/styles/components/section-howto"
-@import "assets/styles/components/section-credentials"
-@import "assets/styles/components/section-newsletter"
-@import "assets/styles/components/hero-mail"
-@import "assets/styles/components/animated-highlighter"
+  @import "assets/styles/style"
 </style>
