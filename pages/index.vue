47d35787e2c9d7cdd37fc6867dc896aa6af70fc8
Fred Guth <fredguth@fredguth.com>
2020-09-10 21:06:02 -0300

    now config

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 main
-  <OfdHeader />
+  //- <OfdHeader />
   section.section-problem
     .content
       h2.title Word™ document collaboration via email is a mess
@@ -28,28 +28,39 @@ main
       img(src="../assets/logo.svg")
 
     .content
-      h1.title You don't need to ditch email to improve your workflow.
+      h1.title Email friendly Word™ collaboration  for pros.
       //- <span class="-strikethrough">dis</span>control
       .branding.-mobile
         img(src="../assets/logo.svg")
       p Our platform compare versions shared by email to let you spot and prevent unwanted changes. So you can focus on content, finally!
-      a.ofd-button(href="#signup") See it in action
+      a.ofd-button(href="#demo") See it in action
       //- <SubmitEmail text='See it in action' subtitle='* Free. No credit card required.'/>
 
-  section.section-howto
-    .magic
-      img(src="../assets/magician.png")
-    h2.title Just <i>carbon copy </i> <a mailto="magic@onefinaldoc.com" style="word-wrap: break-word;">magic@onefinaldoc.com</a> and reply-to-all... to doubt your own eyes
-      .hero-mail
-        header.header
-          h4 <em>To:</em> andrew@part-one.com, clara@part-two.com
-          h4 <em>From:</em> sarah@reallyhappy.com
-          h4 <em>Subject:</em> Important Contract
-          h4.animated-highlighter <em>Cc:</em> magic@onefinaldoc.com
-      p We auto-upload all documents to your personal repository where is dead simple to identify changes and compare versions.
+  //- section.section-howto
+  //-   .magic
+  //-     img(src="../assets/magician.png")
+  //-   h2.title Just <i>carbon copy </i> <a mailto="magic@onefinaldoc.com" style="word-wrap: break-word;">magic@onefinaldoc.com</a> and reply-to-all... to doubt your own eyes
+  //-     .hero-mail
+  //-       header.header
+  //-         h4 <em>To:</em> andrew@part-one.com, clara@part-two.com
+  //-         h4 <em>From:</em> sarah@reallyhappy.com
+  //-         h4 <em>Subject:</em> Important Contract
+  //-         h4.animated-highlighter <em>Cc:</em> magic@onefinaldoc.com
+  //-     p We auto-upload all documents to your personal repository where is dead simple to identify changes and compare versions.
+
   section#signup.section-newsletter
     h2.title Say goodbye to chaos 👋
-    <SubmitEmail text='Get your invitation' subtitle='* Free. No credit card required.'/>
+    h3 Company
+    p About
+    p Press
+    p Blog
+    p Contact
+    h3 Legal
+    p Terms of Service
+    p Privacy Policy
+    p Imprint
+
+    //- <SubmitEmail text='Get your invitation' subtitle='* Free. No credit card required.'/>
     //- p Already a customer? <a href="/signin">Sign in</a>
 </template>
 
